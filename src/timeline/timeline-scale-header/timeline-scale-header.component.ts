bbdc526a9be419eef8c4c16f5f552f4a48500236
Андрій Крашівський <a.krashivskyi@avidi.tech>
2022-11-24 15:00:41 +0200

    performance improvements for scale header

--- src/timeline/timeline-scale-header/timeline-scale-header.component.ts
+++ src/timeline/timeline-scale-header/timeline-scale-header.component.ts
@@ -9,9 +9,20 @@ import { IScale, IScaleColumn, IScaleGroup } from '../scale-generator/models';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimelineScaleHeaderComponent {
+  private _zoom: ITimelineZoom;
+  public columnNameMode: keyof Pick<IScaleColumn, 'shortName' | 'longName' | 'name'>;
+
   @Input() height: number;
-  @Input() zoom: ITimelineZoom | undefined;
   @Input() scale: IScale;
+
+  @Input() set zoom(zoom: ITimelineZoom | undefined) {
+    this._zoom = zoom;
+    this._setNameMode();
+  }
+
+  get zoom(): ITimelineZoom {
+    return this._zoom;
+  }
 
   get headerGroups(): IScaleGroup[] {
     return this.scale?.groups ?? [];
@@ -21,17 +32,16 @@ export class TimelineScaleHeaderComponent {
     return this.scale?.columns ?? [];
   }
 
-  getColumnName(column: IScaleColumn): string {
-    if (this.zoom.columnWidth < 65 && column.shortName)
-      return column.shortName;
-
-    if (this.zoom.columnWidth > 180 && column.longName)
-      return column.longName;
-
-    return column.name;
-  }
-
   trackById(index: number, item: IIdObject): number | string {
     return item.id;
+  }
+
+  private _setNameMode(): void {
+    if (this._zoom.columnWidth < 65)
+      this.columnNameMode = 'shortName';
+    else if (this._zoom.columnWidth > 180)
+      this.columnNameMode = 'longName';
+    else
+      this.columnNameMode = 'name';
   }
 }
